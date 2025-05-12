56993ef3be193d293b60c029052d99e64af62f54
arrithniusjr <khulisoravhuravhu@gmail.com>
2025-05-13 01:06:10 +0200

    about section added on the homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -144,6 +144,24 @@
                 </div>
             </div>
         </section>
+        <section class="about_summ">
+            <div class="main_container">
+                <div class="image_block block">
+                    <img src="/images/image5-min-1.png"/>
+                </div>
+                <div class="about_brief block">
+                    <div class="title_container">
+                        <h2>About Us</h2>
+                    </div>
+                    <div class="dscpt">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <div class="bttn">
+                        <button>Read More</button>
+                    </div>
+                </div>
+            </div>
+        </section>
         <section class="rcnt_vd">
             <div class="main_container">
                 <div class="title_container">

93380e7296b2d819ee3b3a3b942e8ea9873f0f08
Khuliso J. Ravhuravhu <khulisoravhuravhu@gmail.com>
2025-06-30 12:31:05 +0200

    must verify email

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 
-class Client extends Authenticatable
+class Client extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
